667a722aa1202f1f3a0118b15b1bb29d5930044c
ivan-aksamentov <ivan.aksamentov@gmail.com>
2023-08-31 17:23:03 +0200

    feat: rebrand to nextstrain
    Rebranding after transferring from neherlab

--- web/src/constants.ts
+++ web/src/constants.ts
@@ -1,8 +1,8 @@
 export const PROJECT_NAME = 'Frequencies' as const
 export const PROJECT_DESCRIPTION = 'Pathogen variant frequencies' as const
 export const COPYRIGHT_YEAR_START = 2023 as const
-export const COMPANY_NAME = 'NeherLab' as const
-export const TEAM_NAME = 'neherlab' as const
+export const COMPANY_NAME = 'Nextstrain' as const
+export const TEAM_NAME = 'nextstrain' as const
 
 export const DOMAIN = process.env.DOMAIN ?? ''
 export const DOMAIN_STRIPPED = process.env.DOMAIN_STRIPPED ?? ''
@@ -11,10 +11,10 @@ export const SOCIAL_IMAGE_WIDTH = '1200'
 export const SOCIAL_IMAGE_HEIGHT = '630'
 export const URL_MANIFEST_JSON = `${DOMAIN}/manifest.json`
 
-export const URL_GITHUB = 'https://github.com/neherlab/flu_frequencies' as const
-export const URL_GITHUB_FRIENDLY = 'github.com/neherlab/flu_frequencies' as const
-export const URL_GITHUB_ISSUES = 'https://github.com/neherlab/flu_frequencies/issues' as const
-export const URL_GITHUB_ISSUES_FRIENDLY = 'github.com/neherlab/flu_frequencies/issues' as const
+export const URL_GITHUB = 'https://github.com/nextstrain/flu_frequencies' as const
+export const URL_GITHUB_FRIENDLY = 'github.com/nextstrain/flu_frequencies' as const
+export const URL_GITHUB_ISSUES = 'https://github.com/nextstrain/flu_frequencies/issues' as const
+export const URL_GITHUB_ISSUES_FRIENDLY = 'github.com/nextstrain/flu_frequencies/issues' as const
 
 export const TWITTER_USERNAME_RAW = 'richardneher' as const
 export const TWITTER_USERNAME_FRIENDLY = `@${TWITTER_USERNAME_RAW}`
